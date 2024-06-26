4702e559c957611f9880b3099bca4bcef3b4df00
knva <txt_ch@163.com>
2024-06-26 11:51:36 +0800

    update jsondecode and update ipaddress input

--- app/src/main/java/org/aize/u30air/MainActivity.kt
+++ app/src/main/java/org/aize/u30air/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -72,16 +73,8 @@ fun MainScreen(context: Context) {
     val viewModel: MainViewModel = remember { MainViewModelFactory(context) }.let { factory ->
         viewModel(factory = factory)
     }
-    // 初始化SharedPreferences
-    val sharedPref = LocalContext.current.getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE)
-    val savedIpAddress = remember {
-        mutableStateOf(sharedPref.getString("ipAddress", ""))
-    }
-//MainScreen画一个界面，所有元素居中，第一行 ip 让用户输入并且持久保存下来，下次打开还是这个，第二行 电量：数值 % 第三行流量：数值GB
-//第四行按钮，点击后获取当前的电量和流量，然后发送到服务器，服务器返回一个json，显示在界面上
-//第五行显示服务器返回的json
 
-    val ipAddress = remember { mutableStateOf("") }
+    val ipAddress = viewModel.ipAddress.collectAsState()
     val battery = viewModel.battery.observeAsState()
     val traffic = viewModel.traffic.observeAsState()
     //充电状态
@@ -98,6 +91,9 @@ fun MainScreen(context: Context) {
 //    //信号强度
 //    private val _Z5g_rsrp = MutableLiveData<String>()
     val Z5g_rsrp = viewModel.Z5g_rsrp.observeAsState()
+
+    val network_lte_rsrp = viewModel.network_lte_rsrp.observeAsState()
+
 //
 //    //realtime_time
 //    private val _realtime_time = MutableLiveData<String>()
@@ -111,11 +107,7 @@ fun MainScreen(context: Context) {
 //    private val _flux_realtime_tx_thrpt= MutableLiveData<String>()
     val fluxRealtimeTx = viewModel.flux_realtime_tx_thrpt.observeAsState()
 
-    if(savedIpAddress.value != ""){
-        ipAddress.value = savedIpAddress.value!!
-    }else{
-        ipAddress.value = "192.168.0.1"
-    }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -131,19 +123,11 @@ fun MainScreen(context: Context) {
 
         TextField(
             value = ipAddress.value,
-            onValueChange = { ipAddress.value = it },
+            onValueChange = { viewModel.saveIpAddress(it) },
             label = { Text("后台IP地址：") },
             modifier = Modifier.padding(top = 16.dp)
         )
-        Button(onClick = { /* 保存IP地址的逻辑 */
-            //保存ip地址 ，下次加载读取
-            with(sharedPref.edit()) {
-                putString("ipAddress", ipAddress.value)
-                apply()
-            }
-        }) {
-            Text("保存")
-        }
+
         // 电量进度条
         var batteryValue = battery.value?.toIntOrNull()
         if(batteryValue == null){
@@ -161,12 +145,17 @@ fun MainScreen(context: Context) {
         val realtimeHour = realtime_time.value?.toLongOrNull()?.div(3600)
         val realtimeMin = realtime_time.value?.toLongOrNull()?.rem(3600)?.div(60)
         val realtimeSec = realtime_time.value?.toLongOrNull()?.rem(60)
+
+        Text(text = "电量：${battery.value} %", modifier = Modifier.padding(top = 16.dp))
+        if(charging.value == true){
+            Text(text = "充电状态：正在充电", modifier = Modifier.padding(top = 16.dp))
+
+        }else{
+            Text(text = "充电状态：未充电", modifier = Modifier.padding(top = 16.dp))
+        }
         Text(text = "开机时间：${realtimeHour}小时${realtimeMin}分${realtimeSec}秒", modifier = Modifier.padding(top = 16.dp))
 
 
-
-        Text(text = "电量：${battery.value} %", modifier = Modifier.padding(top = 16.dp))
-        Text(text = "充电状态：${charging.value}", modifier = Modifier.padding(top = 16.dp))
 
         // 流量进度条
         var trafficValue = traffic.value?.toFloatOrNull()
@@ -187,7 +176,18 @@ fun MainScreen(context: Context) {
         // 网络制式，信号强度 运营商
         Text(text = "网络制式：${network_type.value}", modifier = Modifier.padding(top = 16.dp))
         Text(text = "运营商：${network_provider_fullname.value}", modifier = Modifier.padding(top = 16.dp))
-        Text(text = "信号强度：${Z5g_rsrp.value} dbm", modifier = Modifier.padding(top = 16.dp))
+        //5g没有就用4g
+        if(network_type.value.equals("5G")) {
+            Text(
+                text = "信号强度：${Z5g_rsrp.value} dbm",
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }else{
+            Text(
+                text = "信号强度：${network_lte_rsrp.value} dbm",
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
         //上传 下载流量显示，除以1024/s
         val rx = fluxRealtimeRx.value?.toFloatOrNull() ?: 0f
         val tx = fluxRealtimeTx.value?.toFloatOrNull() ?: 0f
--- app/src/main/java/org/aize/u30air/MainViewModel.kt
+++ app/src/main/java/org/aize/u30air/MainViewModel.kt
@@ -13,6 +13,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
@@ -22,6 +24,7 @@ import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.util.concurrent.ConcurrentHashMap
+import org.json.JSONObject
 
 class MyCookieJar : CookieJar {
     private val cookieStore = ConcurrentHashMap<String, List<Cookie>>()
@@ -37,12 +40,12 @@ class MyCookieJar : CookieJar {
 
 
 class MainViewModel(private val context: Context) : ViewModel() {
-    private val sharedPreferences: SharedPreferences =
-        context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+    private val sharedPref: SharedPreferences = context.getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE)
 
-    val ipAddress: String by lazy {
-        sharedPreferences.getString("ipAddress", "192.168.0.1") ?: "192.168.0.1"
-    }
+    private val _ipAddress = MutableStateFlow(sharedPref.getString("ipAddress", "") ?: "")
+    val ipAddress: StateFlow<String> = _ipAddress
+
+
     private val _battery = MutableLiveData<String>()
     val battery: LiveData<String> = _battery
 
@@ -65,6 +68,10 @@ class MainViewModel(private val context: Context) : ViewModel() {
     private val _Z5g_rsrp = MutableLiveData<String>()
     val Z5g_rsrp: LiveData<String> = _Z5g_rsrp
 
+    //network_lte_rsrp
+    private val _network_lte_rsrp = MutableLiveData<String>()
+    val network_lte_rsrp: LiveData<String> = _network_lte_rsrp
+
     //realtime_time
     private val _realtime_time = MutableLiveData<String>()
     val realtime_time: LiveData<String> = _realtime_time
@@ -83,22 +90,35 @@ class MainViewModel(private val context: Context) : ViewModel() {
     init {
         startTimer()
     }
-    private  suspend fun sendGetRequest(ipAddress: String): String {
+    fun saveIpAddress(ip: String) {
+        viewModelScope.launch {
+            with(sharedPref.edit()) {
+                putString("ipAddress", ip)
+                apply()
+            }
+            _ipAddress.value = ip
+        }
+    }
+    private  suspend fun sendGetRequest(myipAddress: String): String {
+        var tmpaddress = myipAddress
+        if(tmpaddress==""){
+            tmpaddress = "192.168.0.1"
+        }
         val client = OkHttpClient.Builder()
             .cookieJar(MyCookieJar())
             .build()
 
-        val url1 = "http://$ipAddress/"
+        val url1 = "http://$tmpaddress/"
         val request1 = Request.Builder()
             .url(url1)
             .build()
 
-        val url = "http://$ipAddress/goform/goform_get_cmd_process?multi_data=1&isTest=true&sms_received_flag_flag=0&sts_received_flag_flag=0&cmd=battery_charging%2Cbattery_vol_percent%2Cbattery_value%2Cbattery_pers%2Cflux_monthly_rx_bytes%2Cflux_monthly_tx_bytes%2Cnetwork_type%2Cnetwork_provider_fullname%2CZ5g_rsrp%2Crealtime_time%2Cflux_realtime_rx_thrpt%2Cflux_realtime_tx_thrpt&_=1719295421412"
+        val url = "http://$tmpaddress/goform/goform_get_cmd_process?multi_data=1&isTest=true&sms_received_flag_flag=0&sts_received_flag_flag=0&cmd=battery_charging%2Cbattery_vol_percent%2Cbattery_value%2Cbattery_pers%2Cflux_monthly_rx_bytes%2Cflux_monthly_tx_bytes%2Cnetwork_type%2Cnetwork_provider_fullname%2CZ5g_rsrp%2Crealtime_time%2Cflux_realtime_rx_thrpt%2Cflux_realtime_tx_thrpt%2Cnetwork_lte_rsrp&_=1719295421412"
         val request = Request.Builder()
             .url(url) .header("accept", "application/json, text/javascript, */*; q=0.01")
             .header("accept-language", "zh-CN,zh;q=0.9,en;q=0.8")
             .header("x-requested-with", "XMLHttpRequest")
-            .header("referer", "http://192.168.0.1/index.html")
+            .header("referer", "http://"+tmpaddress+"/index.html")
             .build()
 
         // 使用 async 并发执行两个请求
@@ -117,8 +137,8 @@ class MainViewModel(private val context: Context) : ViewModel() {
             repeat(Int.MAX_VALUE) {
                 try {
                     delay(30) // 每5秒更新一次
-//                      val jsondata ="{\"battery_charging\":\"1\",\"battery_vol_percent\":\"100\",\"battery_value\":\"100\",\"battery_pers\":\"\",\"monthly_tx_bytes\":2675052511,\"monthly_rx_bytes\":23600180829,\"monthly_time\":359779,\"flux_monthly_rx_bytes\":23600180829,\"flux_monthly_tx_bytes\":2675052511,\"flux_monthly_time\":359779}"
-                    val jsondata = sendGetRequest(ipAddress)
+                  // val jsondata ="{\"battery_charging\":\"1\",\"battery_vol_percent\":\"100\",\"battery_value\":\"100\",\"battery_pers\":\"\",\"monthly_tx_bytes\":2675052511,\"monthly_rx_bytes\":23600180829,\"monthly_time\":359779,\"flux_monthly_rx_bytes\":23600180829,\"flux_monthly_tx_bytes\":2675052511,\"flux_monthly_time\":359779}"
+                    val jsondata = sendGetRequest(ipAddress.value)
                     _lastJson.value=jsondata
                     decodeJson(jsondata)
                 }catch ( e: Exception){
@@ -149,47 +169,48 @@ class MainViewModel(private val context: Context) : ViewModel() {
     }
 
     private fun decodeJson(jsondata: String) {
-        //是否充电
-        val chargingValue =
-            jsondata.substringAfter("battery_charging").substringAfter(":").substringBefore(",")
-                .replace("\"", "").toInt()
-        //{"battery_charging":"1","battery_vol_percent":"100","battery_value":"100","battery_pers":"","monthly_tx_bytes":2675052511,"monthly_rx_bytes":23600180829,"monthly_time":359779,"flux_monthly_rx_bytes":23600180829,"flux_monthly_tx_bytes":2675052511,"flux_monthly_time":359779}
-        val batteryValue =
-            jsondata.substringAfter("battery_value").substringAfter(":").substringBefore(",")
-                .replace("\"", "")
+        val jsonObject = JSONObject(jsondata)
+
+        // 是否充电
+        val chargingValue = jsonObject.getString("battery_charging").toInt()
+
+        // 电池值
+        val batteryValue = jsonObject.getString("battery_value")
+
         // 流量是rx+tx
-        val trafficValue =
-            (jsondata.substringAfter("monthly_rx_bytes").substringAfter(":").substringBefore(",")
-                .toLong() +
-                    jsondata.substringAfter("monthly_tx_bytes").substringAfter(":")
-                        .substringBefore(",").toLong()) / 1024 / 1024 / 1024
-        val network_type =
-            jsondata.substringAfter("network_type").substringAfter(":").substringBefore(",")
-                .replace("\"", "")
-        val network_provider_fullname =
-            jsondata.substringAfter("network_provider_fullname").substringAfter(":")
-                .substringBefore(",").replace("\"", "")
-        val Z5g_rsrp = jsondata.substringAfter("Z5g_rsrp").substringAfter(":").substringBefore(",")
-            .replace("\"", "")
-        val realtime_time =
-            jsondata.substringAfter("realtime_time").substringAfter(":").substringBefore(",")
-                .replace("\"", "")
-        val flux_realtime_rx_thrpt =
-            jsondata.substringAfter("flux_realtime_rx_thrpt").substringAfter(":")
-                .substringBefore(",").replace("\"", "").replace("}", "")
-        val flux_realtime_tx_thrpt =
-            jsondata.substringAfter("flux_realtime_tx_thrpt").substringAfter(":")
-                .substringBefore(",").replace("\"", "")
+        val trafficValue = (jsonObject.getLong("monthly_rx_bytes") + jsonObject.getLong("monthly_tx_bytes")) / 1024 / 1024 / 1024
+
+        // 网络类型
+        val networkType = jsonObject.optString("network_type")
+
+        // 网络提供商全名
+        val networkProviderFullName = jsonObject.optString("network_provider_fullname")
+
+        // 5g rsrp
+        val Z5gRsrp = jsonObject.optString("Z5g_rsrp")
+
+        //4g rsrp
+        val networkLteRsrp = jsonObject.optString("network_lte_rsrp")
+
+        // 实时时间
+        val realtimeTime = jsonObject.optString("realtime_time")
+
+        // 实时接收吞吐量
+        val fluxRealtimeRxThrpt = jsonObject.optString("flux_realtime_rx_thrpt").replace("}", "")
+
+        // 实时发送吞吐量
+        val fluxRealtimeTxThrpt = jsonObject.optString("flux_realtime_tx_thrpt")
 
         // 模拟更新电池和流量数据
-        _battery.value = batteryValue.toString()
+        _battery.value = batteryValue
         _traffic.value = trafficValue.toString()
         _charging.value = chargingValue == 1
-        _network_type.value = network_type
-        _network_provider_fullname.value = network_provider_fullname
-        _Z5g_rsrp.value = Z5g_rsrp
-        _realtime_time.value = realtime_time
-        _flux_realtime_rx_thrpt.value = flux_realtime_rx_thrpt
-        _flux_realtime_tx_thrpt.value = flux_realtime_tx_thrpt
+        _network_type.value = networkType
+        _network_provider_fullname.value = networkProviderFullName
+        _Z5g_rsrp.value = Z5gRsrp
+        _network_lte_rsrp.value = networkLteRsrp
+        _realtime_time.value = realtimeTime
+        _flux_realtime_rx_thrpt.value = fluxRealtimeRxThrpt
+        _flux_realtime_tx_thrpt.value = fluxRealtimeTxThrpt
     }
 }
